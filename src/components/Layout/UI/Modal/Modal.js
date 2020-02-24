4e12f198bcca1736c6d056c955a82733e2202f2e
unknown <jadB0y331@gmail.com>
2020-02-24 13:49:44 -0600

    added close button for modals and fixed navbar to top for small devices

--- src/components/Layout/UI/Modal/Modal.js
+++ src/components/Layout/UI/Modal/Modal.js
@@ -6,7 +6,12 @@ class Modal extends Component {
       <>
         <div className="modal-container">
           <div className="modal-backdrop" onClick={this.props.backdropOnClick}></div>
-          <div className="modal-content">{this.props.content}</div>
+          <div className="modal-content">
+            <div className="text-right">
+              <span className="modal-closeModal" onClick={this.props.backdropOnClick}>Close <i class="fas fa-times"></i></span>
+            </div>
+            {this.props.content}
+          </div>
         </div>
       </>
     )
